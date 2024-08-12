4d435ae414904c5eeb891ed53ef5a97e2ef0bd3e
Bry <bry.ashman@clarinspect.com>
2024-08-12 12:40:18 +1200

    add relations

--- app/models/hilti_import.rb
+++ app/models/hilti_import.rb
@@ -1,3 +1,8 @@
 class HiltiImport < ApplicationRecord
   has_one_attached :archive
+  has_one :list_report
+  has_many :floor_plans, dependent: destroy
+  has_many :inspection_images, dependent: destroy
+  has_many :pdf_reports, dependent: destroy
+  has_many :test_reports, dependent: destroy
 end
