158c1db7bf006424be0d232cba142fb6a87809e3
Bry <bry.ashman@clarinspect.com>
2024-08-27 18:27:09 +1200

    wip

---
+++ app/javascript/controllers/marker_controller.js
@@ -0,0 +1,54 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["canvas"]
+    static values = {
+        src: String,
+        ox: Number,
+        oy: Number,
+        ex: Number,
+        ey: Number,
+    }
+
+    connect() {
+        const bounds = this.element.getBoundingClientRect();
+
+        if (this.canvasTarget.getContext) {
+            const ctx = this.canvasTarget.getContext("2d");
+
+            const img = new Image();
+
+            img.addEventListener("load", () => {
+                const h = img.height;
+                const w = img.width;
+
+                this.canvasTarget.width = w;
+                this.canvasTarget.height = h;
+
+                ctx.drawImage(img, 0, 0);
+
+                let r = 10;
+
+                ctx.beginPath()
+                let ox = this.oxValue * w;
+                let oy = this.oyValue * h;
+
+                ctx.ellipse(ox, oy, r, r, 0, 0, 2 * Math.PI);
+                ctx.fillStyle = "#00FF00";
+                ctx.stroke();
+                ctx.fill();
+
+                ctx.beginPath()
+                let ex = this.exValue * w;
+                let ey = this.eyValue * h;
+
+                ctx.ellipse(ex, ey, r, r, 0, 0, 2 * Math.PI);
+                ctx.fillStyle = "#FF0000";
+                ctx.stroke();
+                ctx.fill();
+            });
+
+            img.src = this.srcValue;
+        }
+    }
+}
--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -2,14 +2,60 @@ class Inspection < ApplicationRecord
   belongs_to :hilti_import
   belongs_to :hilti_project
 
+  Marker = Data.define(:origin, :end_point)
+  Point = Data.define(:x, :y) do
+    def -@
+      Point[-self.x, -self.y]
+    end
+
+    def -(other)
+      Point[self.x - other.x, self.y - other.y]
+    end
+
+    def +(other)
+      Point[self.x + other.x, self.y + other.y]
+    end
+
+    def scale(sX, sY)
+      Point[self.x * sX, self.y * sY]
+    end
+
+    def flipY
+      Point[self.x, 1.0-self.y]
+    end
+  end
+
   def floor_plan
-    if marker.present?
+    @floor_plan ||= if marker.present?
       hilti_import.floor_plans.find_by reference: self.marker["attachment_id"]
     end
   end
 
+  def marker_location
+    @marker_location ||= if floor_plan
+      page = floor_plan.page
+      b = Point[page.bx, page.by]
+      t = Point[page.tx, page.ty]
+      s = t - b
+
+      rl = raw_location
+      ro = Point[rl[0], rl[1]]
+      re = Point[rl[2], rl[3]]
+
+      origin = (ro - b).scale(1.0/s.x, 1.0/s.y).flipY
+      end_point = (re - b).scale(1.0/s.x, 1.0/s.y).flipY
+
+      Marker[
+        origin:,
+        end_point:,
+      ]
+    end
+  end
 
 
+  def raw_location
+    marker["location"].split(",").map(&:to_f)
+  end
 
   def build_from_document(doc)
     self.penetration = doc.to_s
